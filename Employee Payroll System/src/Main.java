6caa8c94ea089eccd8cffced812745b524e0537a
mamun-opu <abmamun504@gmail.com>
2023-11-09 06:48:50 -0500

    abstract employee

--- Employee Payroll System/src/Main.java
+++ Employee Payroll System/src/Main.java
@@ -1,5 +1,18 @@
-class Employee{
-    private
+abstract class Employee{
+    private String name;
+    private double id;
+    Employee(String name, double id){
+        this.name = name;
+        this.id = id;
+    }
+    public String getName(){
+        return name;
+    }
+    public double getId(){
+        return id;
+    }
+    abstract double calculateSalary();
+
 }
 
 
