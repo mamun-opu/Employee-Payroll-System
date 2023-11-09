cbd9e8788723987b114e66798a4d7134aceeb201
mamun-opu <abmamun504@gmail.com>
2023-11-09 06:57:27 -0500

    part time and full time employee class

--- Employee Payroll System/src/Main.java
+++ Employee Payroll System/src/Main.java
@@ -1,21 +1,51 @@
 abstract class Employee{
     private String name;
-    private double id;
-    Employee(String name, double id){
+    private int id;
+    Employee(String name, int id){
         this.name = name;
         this.id = id;
     }
     public String getName(){
         return name;
     }
-    public double getId(){
+    public int getId(){
         return id;
     }
     abstract double calculateSalary();
 
 }
 
+class FullTimeEmployee extends Employee{
+    private double salary;
+    FullTimeEmployee(String name, int id, double salary){
+        super(name, id);
+        this.salary = salary;
+    }
 
+    @Override
+    double calculateSalary() {
+        return salary;
+    }
+}
+
+class PartTimeEmployee extends Employee{
+    private double workingHours;
+    private double hourlyPayRate;
+    PartTimeEmployee(String name, int id, double workingHours, double hourlyPayRate){
+        super(name, id);
+        this.workingHours = workingHours;
+        this.hourlyPayRate = hourlyPayRate;
+    }
+
+    @Override
+    double calculateSalary() {
+        return workingHours*hourlyPayRate;
+    }
+}
+
+class EmployeePayRoll{
+
+}
 
 
 
